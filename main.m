f6306aad944e4944a5274fea2bc7834ae5a5ca0e
Jisheng Dai <jsdai.ustc@gmail.com>
2026-05-14 14:52:14 +0800

    Update main.m

--- main.m
+++ main.m
@@ -12,7 +12,6 @@ L = 30; % number of delay taps
 bandwidth = 2e7;
 %%
 [Y, H, sequence_Phi, coefficients,F_truncated] = signal_generation(SNR, interval, N, G, M, L, bandwidth);
-
 H_est = EPA(Y, M,  coefficients,sequence_Phi,F_truncated);
 norm( H - H_est, 'fro' )^2 / norm( H, 'fro' )^2
 
